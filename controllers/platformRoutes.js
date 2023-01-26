235336be35c14019c7a93d3a920b5568f03bd99e
phadeline <112015433+phadeline@users.noreply.github.com>
2023-01-26 15:33:58 -0500

    routes completed

--- controllers/platformRoutes.js
+++ controllers/platformRoutes.js
@@ -1,1 +1,44 @@
 //search by a single platform or all the platforms in the database
+const router = require('express').Router();
+const { Platform, Review, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// get all platforms
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const platformData = await Platform
+      .findAll
+      // {include: [{ model: Review}, {model: User }],}
+      ()
+      .catch((err) => {
+        res.json(err);
+      });
+    const platforms = platformData.map((platform) =>
+      platform.get({ plain: true })
+    );
+    res.render('all-platforms', { platforms });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get one platform
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const platformData = await Platform.findByPk(
+      req.params.id,
+      // {include: [{ model: Review, include: [{ model: User }] }],}
+    ).catch((err) => {
+      res.json(err);
+    });
+    if (!platformData) {
+      res.status(404).json({ message: 'no platform found with this id!' });
+    }
+    const platform = platformData.get({ plain: true });
+    res.render('platform', { platform });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
