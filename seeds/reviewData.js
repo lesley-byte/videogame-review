3fe9f4060da0617bebcef136419eeabda2e5cc86
Joey Douek <joeyblue27@gmail.com>
2023-01-25 21:03:18 -0500

    added review_text

--- seeds/reviewData.js
+++ seeds/reviewData.js
@@ -3,18 +3,21 @@ const { Review } = require('../models');
 
 const reviewData = [
     {
+        review_title: 'awesome game!',
         review_text: 'This game is awesome!',
         user_id: 1,
         game_id: 1,
         review_date: '2021-01-01',
     },
     {
+        review_title: 'awesome game!',
         review_text: 'This game is awesome!',
         user_id: 1,
         game_id: 1,
         review_date: '2021-01-01',
     },
     {
+        review_title: 'awesome game!',
         review_text: 'This game is awesome!',
         user_id: 2,
         game_id: 2,
