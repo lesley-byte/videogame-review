1fd25f8f9bab12165c9d8a01354e80dae84ee007
phadeline <112015433+phadeline@users.noreply.github.com>
2023-02-05 17:33:57 -0500

    reviews

--- seeds/reviewData.js
+++ seeds/reviewData.js
@@ -57,23 +57,41 @@ const reviewData = [
     user_id: 3,
     game_id: 5,
     review_date: '2021-01-01',
-},
-{
+  },
+  {
     review_rating: 5,
     review_title: 'such a cool game',
     review_text: 'I enjoyed playing this with my friends!',
     user_id: 1,
     game_id: 6,
     review_date: '2021-01-01',
-},
-{
+  },
+  {
     review_rating: 3,
     review_title: 'not bad but not great!',
-    review_text: 'I expeected better but at the same time I was not disappointed',
+    review_text:
+      'I expeected better but at the same time I was not disappointed',
     user_id: 2,
     game_id: 7,
     review_date: '2021-01-01',
-},
+  },
+  {
+    review_rating: 4,
+    review_title: 'just WOW!',
+    review_text: 'I was blown away. Did not expect it to be this good!',
+    user_id: 4,
+    game_id: 8,
+    review_date: '2021-01-01',
+  },
+  {
+    review_rating: 1,
+    review_title: 'This game was a MONEY Grab!!',
+    review_text:
+      'I can not believe that they are actually selling this as a game! SMH',
+    user_id: 1,
+    game_id: 9,
+    review_date: '2021-01-01',
+  },
 ];
 
 const seedReviews = () => Review.bulkCreate(reviewData);
